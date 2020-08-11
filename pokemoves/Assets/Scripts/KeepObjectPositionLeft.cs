e9f830c79472cf00cd9920873a7b968fc64d2117
snakeslayer22 <66408223+snakeslayer22@users.noreply.github.com>
2020-08-11 19:31:03 +1000

    MC animations and Start Cave Tiles

---
+++ pokemoves/Assets/Scripts/KeepObjectPositionLeft.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepObjectPositionLeft : StateMachineBehaviour{
+
+    Transform Arm;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Arm = GameObject.FindGameObjectWithTag("arm").transform;
+        Arm.transform.localPosition = new Vector3(0.01f, 0.12f, -6);
+    }
+}
---
+++ pokemoves/Assets/Scripts/SetObjectPositionLeft.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetObjectPositionLeft : StateMachineBehaviour{
+
+    Transform Arm;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Arm = GameObject.FindGameObjectWithTag("arm").transform;
+        Arm.transform.localPosition = new Vector3(0.01f, 0.12f, -6);
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Arm.transform.localPosition = new Vector3(0, 0, -6);
+    }
+}
